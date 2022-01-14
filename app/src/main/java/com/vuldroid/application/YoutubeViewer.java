1e47c4fe3a8312dd8c22b8db5a5e52d09f1b0cfe
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2022-01-14 15:57:33 +0530

    Update

--- app/src/main/java/com/vuldroid/application/YoutubeViewer.java
+++ app/src/main/java/com/vuldroid/application/YoutubeViewer.java
@@ -2,6 +2,7 @@ package com.vuldroid.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -12,14 +13,17 @@ import android.webkit.WebViewClient;
 
 public class YoutubeViewer extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_youtubeviewer);
-        String lods = "https://youtube.com";
+        String lods = "https://www.google.com";
         WebView vulnerable =(WebView) findViewById(R.id.vulweb);
         WebSettings webSettings = vulnerable.getSettings();
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDomStorageEnabled(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAllowFileAccessFromFileURLs(true);
@@ -27,10 +31,15 @@ public class YoutubeViewer extends AppCompatActivity {
         vulnerable.setWebChromeClient(new WebChromeClient());
         WebViewClientImpl webViewClient = new WebViewClientImpl(this);
         vulnerable.setWebViewClient(webViewClient);
-        if ((getIntent() != null) || getIntent().hasExtra("intent_url")) {
-            vulnerable.loadUrl(getIntent().getStringExtra("intent_url"));
+//        if ((getIntent() != null) || getIntent().hasExtra("intent_url")) {
+//            vulnerable.loadUrl(getIntent().getStringExtra("intent_url"));
+//        }
+//        else{vulnerable.loadUrl(lods);}
+        try{
+            vulnerable.loadUrl(getIntent().getData().toString());
+        } catch (Exception e){
+            vulnerable.loadUrl(lods);
         }
-        else{vulnerable.loadUrl(lods);}
     }
     public class WebViewClientImpl extends WebViewClient {
 
